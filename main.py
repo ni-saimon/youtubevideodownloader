a9b51f237d027f9d8910e7f624ccb3e4bcddafbe
Nafiz Imtiaz Saimon <40791066+ni-saimon@users.noreply.github.com>
2022-10-09 21:54:17 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from pytube import YouTube
 
+# Youtube link input from user
 yt = YouTube(input())
 
-yt.streams.get_by_resolution('720p').download('C:\\Users\\nafiz\\Downloads\\YT')
+# Resolution of the video
+Resolution = '720p'
+
+# Download path
+Path = 'C:\\Users\\nafiz\\Downloads\\YT'
+
+yt.streams.get_by_resolution(Resolution).download(Path)
